bba92d95c75aa7edc62331e796b382c7c8668475
Sergey Shevchenko <sshevchenko@mirantis.com>
2015-08-31 11:01:53 +0300

    Add zones assignment
    Change-Id: I72cb2152672f40bbd90c03c25bcbe9c65338e272

--- deployment_scripts/puppet/modules/standalone_swift/manifests/proxy.pp
+++ deployment_scripts/puppet/modules/standalone_swift/manifests/proxy.pp
@@ -6,13 +6,20 @@ class standalone_swift::proxy {
 include ceilometer
 include memcached
 
-
-    notice('Fuel plugin swift, standalone_swift puppet module, init.pp')
+    notice('Fuel plugin swift, standalone_swift puppet module, proxy.pp')
 
     $network_metadata      = hiera('network_metadata')
     $swift_hash            = hiera('swift_hash')
- #   $swift_nodes           = filter_nodes(hiera('nodes_hash'),'role','swift-storage')
     $swift_nodes           = get_nodes_hash_by_roles($network_metadata, ['swift-storage'])
+
+    $swift_nodes_fix_zone = inline_template("<%=
+      $swift_nodes['nodes'].each do |key, array|
+        if array['user_node_name'][/^.*zone-(\d*)$/, 1]  # If user assigned node name contains 'zone-N' where N is a number
+          array['swift_zone'] = array['user_node_name'][/^.*zone-(\d*)$/, 1]   # Substitute swift_zone number with this number
+        end
+      end
+    %>")
+
     $primary_swift         = filter_nodes(hiera('nodes_hash'),'role','primary-swift-proxy')
     #$master_swift_proxy_ip = $primary_swift[0]['storage_address']
     $proxy_port            = pick($swift_hash['proxy_port'], '8080')
@@ -29,13 +36,13 @@ include memcached
     $loopback_size         = pick($swift_hash['loopback_size'], '5243780')
     $storage_type          = pick($swift_hash['storage_type'], false)
 
-#Keystone settings
-$service_endpoint        = hiera('service_endpoint')
-$keystone_user           = pick($swift_hash['user'], 'swift')
-$keystone_password       = pick($swift_hash['user_password'], 'passsword')
-$keystone_tenant         = pick($swift_hash['tenant'], 'services')
-$keystone_protocol       = pick($swift_hash['auth_protocol'], 'http')
-$region                  = hiera('region', 'RegionOne')
+    #Keystone settings
+    $service_endpoint        = hiera('service_endpoint')
+    $keystone_user           = pick($swift_hash['user'], 'swift')
+    $keystone_password       = pick($swift_hash['user_password'], 'passsword')
+    $keystone_tenant         = pick($swift_hash['tenant'], 'services')
+    $keystone_protocol       = pick($swift_hash['auth_protocol'], 'http')
+    $region                  = hiera('region', 'RegionOne')
 
     $proxies             = filter_nodes_nonstrict(hiera('nodes_hash'),'user_node_name','^swift-proxy-(primary-)?\d*$')
     $swift_proxies       = nodes_to_hash($proxies,'name','internal_address')
@@ -44,10 +51,10 @@ $region                  = hiera('region', 'RegionOne')
     $sto_net = $network_scheme['endpoints'][$network_scheme['roles']['storage']]['IP']
     $man_net = $network_scheme['endpoints'][$network_scheme['roles']['management']]['IP']
 
-  $master_swift_proxy_nodes      = get_nodes_hash_by_roles($network_metadata, ['primary-swift-proxy'])
-  $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
-  $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
-  $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
+    $master_swift_proxy_nodes      = get_nodes_hash_by_roles($network_metadata, ['primary-swift-proxy'])
+    $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
+    $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
+    $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
 
     # Configure networking on a node, during a stage prior to main
     prepare_network_config(hiera('network_scheme'))
@@ -63,57 +70,47 @@ $region                  = hiera('region', 'RegionOne')
 
     $node_role = hiera('role')
     $primary_proxy            = $node_role ? { 'primary-swift-proxy' => true, default =>false } 
-    notice($primary_proxy)
-    # If the manifest is currently running on a primary proxy set $primary_proxy
-    #  $primary_proxy = true
-    #} else {
-    #  $primary_proxy = false
-    #}
 
     # Generate rings on a primary controller node
     if $primary_proxy {
       ring_devices {'all':
-        storages => $swift_nodes,
+        storages => $swift_nodes_fix_zone,
         require  => Class['swift'],
       }
     }
 
+    class { 'openstack::swift::proxy':
+    swift_user_password            => $swift_hash['user_password'],
+    swift_proxies_cache            => $memcaches_addr_list,
+    ring_part_power                => $ring_part_power,
+    primary_proxy                  => $primary_proxy,
+    swift_proxy_local_ipaddr       => $swift_api_ipaddr,
+    swift_replication_local_ipaddr => $swift_storage_ipaddr,
+    master_swift_proxy_ip          => $master_swift_proxy_ip,
+    master_swift_replication_ip    => $master_swift_replication_ip,
+    proxy_port                     => $proxy_port,
+    debug                          => $debug,
+    verbose                        => $verbose,
+    log_facility                   => 'LOG_SYSLOG',
+    ceilometer                     => hiera('use_ceilometer',false),
+    ring_min_part_hours            => $ring_min_part_hours,
+    admin_user                     => $keystone_user,
+    admin_tenant_name              => $keystone_tenant,
+    admin_password                 => $keystone_password,
+    auth_host                      => $service_endpoint,
+    auth_protocol                  => $keystone_protocol,
+    } ->
 
+    package { 'fuel-ha-utils':
+      ensure => installed,
+    } ->
 
-      class { 'openstack::swift::proxy':
-      swift_user_password            => $swift_hash['user_password'],
-      swift_proxies_cache            => $memcaches_addr_list,
-      ring_part_power                => $ring_part_power,
-      primary_proxy                  => $primary_proxy,
-      swift_proxy_local_ipaddr       => $swift_api_ipaddr,
-      swift_replication_local_ipaddr => $swift_storage_ipaddr,
-      master_swift_proxy_ip          => $master_swift_proxy_ip,
-      master_swift_replication_ip    => $master_swift_replication_ip,
-      proxy_port                     => $proxy_port,
-      debug                          => $debug,
-      verbose                        => $verbose,
-      log_facility                   => 'LOG_SYSLOG',
-      ceilometer                     => hiera('use_ceilometer',false),
-      ring_min_part_hours            => $ring_min_part_hours,
-      admin_user                     => $keystone_user,
-      admin_tenant_name              => $keystone_tenant,
-      admin_password                 => $keystone_password,
-      auth_host                      => $service_endpoint,
-      auth_protocol                  => $keystone_protocol,
-      } ->
-
-      package { 'fuel-ha-utils':
-        ensure => installed,
-      } ->
-
-      class { 'openstack::swift::status':
-        endpoint    => "http://${storage_address}:${proxy_port}",
-        vip         => $management_vip,
-        only_from   => "127.0.0.1 240.0.0.2 ${sto_net} ${man_net}",
-        con_timeout => 5
-      }
-
-    
+    class { 'openstack::swift::status':
+      endpoint    => "http://${storage_address}:${proxy_port}",
+      vip         => $management_vip,
+      only_from   => "127.0.0.1 240.0.0.2 ${sto_net} ${man_net}",
+      con_timeout => 5
+    }
 
     # setup a cronjob to rebalance and repush rings periodically
     class { 'openstack::swift::rebalance_cronjob':
@@ -121,8 +118,6 @@ $region                  = hiera('region', 'RegionOne')
       master_swift_replication_ip => $master_swift_replication_ip,
       primary_proxy         => $primary_proxy,
     }
-
-
 
 }
 
--- deployment_scripts/puppet/modules/standalone_swift/manifests/storage.pp
+++ deployment_scripts/puppet/modules/standalone_swift/manifests/storage.pp
@@ -6,12 +6,10 @@ class standalone_swift::storage {
 include ceilometer
 include memcached
 
-
-    notice('Fuel plugin swift, standalone_swift puppet module, init.pp')
+    notice('Fuel plugin swift, standalone_swift puppet module, storage.pp')
 
     $network_metadata      = hiera('network_metadata')
     $swift_hash            = hiera('swift_hash')
- #   $swift_nodes           = filter_nodes(hiera('nodes_hash'),'role','swift-storage')
     $swift_nodes           = get_nodes_hash_by_roles($network_metadata, ['swift-storage'])
     $primary_swift         = filter_nodes(hiera('nodes_hash'),'role','primary-swift-proxy')
     #$master_swift_proxy_ip = $primary_swift[0]['storage_address']
@@ -29,13 +27,18 @@ include memcached
     $loopback_size         = pick($swift_hash['loopback_size'], '5243780')
     $storage_type          = pick($swift_hash['storage_type'], false)
 
-#Keystone settings
-$service_endpoint        = hiera('service_endpoint')
-$keystone_user           = pick($swift_hash['user'], 'swift')
-$keystone_password       = pick($swift_hash['user_password'], 'passsword')
-$keystone_tenant         = pick($swift_hash['tenant'], 'services')
-$keystone_protocol       = pick($swift_hash['auth_protocol'], 'http')
-$region                  = hiera('region', 'RegionOne')
+    # Test if storage node name has zone assignment
+    validate_re($node['user_node_name'], '^.*zone-\d*$', 'Storage node does not have zone assignment in the user defined node name')
+    # Fetch zone number from user_node_name
+    $swift_zone = inline_template("<%= $node['user_node_name'][/^.*zone-(\d*)$/, 1] %>")
+
+    #Keystone settings
+    $service_endpoint        = hiera('service_endpoint')
+    $keystone_user           = pick($swift_hash['user'], 'swift')
+    $keystone_password       = pick($swift_hash['user_password'], 'passsword')
+    $keystone_tenant         = pick($swift_hash['tenant'], 'services')
+    $keystone_protocol       = pick($swift_hash['auth_protocol'], 'http')
+    $region                  = hiera('region', 'RegionOne')
 
     $proxies             = filter_nodes_nonstrict(hiera('nodes_hash'),'user_node_name','^swift-proxy-(primary-)?\d*$')
     $swift_proxies       = nodes_to_hash($proxies,'name','internal_address')
@@ -44,10 +47,10 @@ $region                  = hiera('region', 'RegionOne')
     $sto_net = $network_scheme['endpoints'][$network_scheme['roles']['storage']]['IP']
     $man_net = $network_scheme['endpoints'][$network_scheme['roles']['management']]['IP']
 
-  $master_swift_proxy_nodes      = get_nodes_hash_by_roles($network_metadata, ['primary-swift-proxy'])
-  $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
-  $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
-  $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
+    $master_swift_proxy_nodes      = get_nodes_hash_by_roles($network_metadata, ['primary-swift-proxy'])
+    $master_swift_proxy_nodes_list = values($master_swift_proxy_nodes)
+    $master_swift_proxy_ip         = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/api'], '\/\d+$', '')
+    $master_swift_replication_ip   = regsubst($master_swift_proxy_nodes_list[0]['network_roles']['swift/replication'], '\/\d+$', '')
 
     # Configure networking on a node, during a stage prior to main
     prepare_network_config(hiera('network_scheme'))
@@ -64,35 +67,34 @@ $region                  = hiera('region', 'RegionOne')
     $node_role = hiera('role')
     $primary_proxy            = $node_role ? { 'primary-swift-proxy' => true, default =>false } 
     
-      file { $swift_partition:
-        ensure => 'directory',
-        owner  => 'swift',
-        group  => 'swift',
-        mode   => '0750',
-      } ->
+    file { $swift_partition:
+      ensure => 'directory',
+      owner  => 'swift',
+      group  => 'swift',
+      mode   => '0750',
+    } ->
 
-      class { 'openstack::swift::storage_node':
-        storage_type          => $storage_type,
-        loopback_size         => $loopback_size,
-        storage_mnt_base_dir  => $swift_partition,
-        storage_devices       => filter_hash($mp_hash,'point'),
-        swift_zone            => $node['swift_zone'],
-        swift_local_net_ip    => $storage_address,
-        master_swift_proxy_ip => $master_swift_proxy_ip,
-        master_swift_replication_ip => $master_swift_replication_ip,
-        sync_rings            => ! $primary_proxy,
-        debug                 => $debug,
-        verbose               => $verbose,
-        log_facility          => 'LOG_SYSLOG',
-      }
-
-
-    # setup a cronjob to rebalance and repush rings periodically
-    class { 'openstack::swift::rebalance_cronjob':
-      ring_rebalance_period => min($ring_min_part_hours * 2, 23),
+    class { 'openstack::swift::storage_node':
+      storage_type          => $storage_type,
+      loopback_size         => $loopback_size,
+      storage_mnt_base_dir  => $swift_partition,
+      storage_devices       => filter_hash($mp_hash,'point'),
+      swift_zone            => $swift_zone,
+      swift_local_net_ip    => $storage_address,
+      master_swift_proxy_ip => $master_swift_proxy_ip,
       master_swift_replication_ip => $master_swift_replication_ip,
-      primary_proxy         => $primary_proxy,
+      sync_rings            => ! $primary_proxy,
+      debug                 => $debug,
+      verbose               => $verbose,
+      log_facility          => 'LOG_SYSLOG',
     }
+
+  # setup a cronjob to rebalance and repush rings periodically
+  class { 'openstack::swift::rebalance_cronjob':
+    ring_rebalance_period => min($ring_min_part_hours * 2, 23),
+    master_swift_replication_ip => $master_swift_replication_ip,
+    primary_proxy         => $primary_proxy,
+  }
 
 }
 
