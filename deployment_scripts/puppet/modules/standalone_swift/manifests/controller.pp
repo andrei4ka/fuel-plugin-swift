69ee475d1dc19e92c71c5ec7166834a05ce27a0d
Sergey Shevchenko <sshevchenko@mirantis.com>
2015-09-08 17:10:32 +0300

    Fix haproxy restart
    Change-Id: If840549e967f508a1510b942bee1888e2cf3e4d3

--- deployment_scripts/puppet/modules/standalone_swift/manifests/controller.pp
+++ deployment_scripts/puppet/modules/standalone_swift/manifests/controller.pp
@@ -3,31 +3,23 @@
 
 class standalone_swift::controller {
 
-  $use_neutron                    = hiera('use_neutron', false)
-  $ceilometer_hash                = hiera_hash('ceilometer',{})
-  $sahara_hash                    = hiera_hash('sahara', {})
-  $murano_hash                    = hiera_hash('murano', {})
-  $storage_hash                   = hiera_hash('storage', {})
-  $controllers                    = hiera('controllers')
-  $haproxy_nodes                  = hiera('haproxy_nodes', $controllers)
-  $rgw_servers                    = undef
+  $internal_virtual_ip = hiera('management_vip')
+  $public_virtual_ip   = hiera('public_vip')
+  $controllers         = hiera('controllers')
+  $swift_proxies       = filter_nodes_nonstrict(hiera('nodes_hash'),'role','^(primary-)?swift-proxy$')
 
-  $swift_proxies = filter_nodes_nonstrict(hiera('nodes_hash'),'role','^(primary-)?swift-proxy$')
+  Haproxy::Service        { use_include => true }
+  Haproxy::Balancermember { use_include => true }
 
-  class { '::openstack::ha::haproxy':
-    controllers              => $haproxy_nodes,
-    public_virtual_ip        => hiera('public_vip'),
-    internal_virtual_ip      => hiera('management_vip'),
-    horizon_use_ssl          => hiera('horizon_use_ssl', false),
-    neutron                  => $use_neutron,
-    queue_provider           => 'rabbitmq',
-    custom_mysql_setup_class => hiera('custom_mysql_setup_class','galera'),
-    swift_proxies            => $swift_proxies,
-    rgw_servers              => $rgw_servers,
-    ceilometer               => $ceilometer_hash['enabled'],
-    sahara                   => $sahara_hash['enabled'],
-    murano                   => $murano_hash['enabled'],
-    is_primary_controller    => hiera('primary_controller'),
+  Openstack::Ha::Haproxy_service {
+    server_names        => filter_hash($controllers, 'name'),
+    ipaddresses         => filter_hash($controllers, 'internal_address'),
+    public_virtual_ip   => $public_virtual_ip,
+    internal_virtual_ip => $internal_virtual_ip,
+  }
+
+  class { '::openstack::ha::swift':
+    servers => $swift_proxies,
   }
 
   service { 'swift-account':
